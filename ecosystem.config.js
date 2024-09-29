f083c442bb2ea036c0d34e1a9c719f5099a67695
상국 배 <partner.ben@kyobobook.com>
2024-09-30 00:18:11 +0900

    feat: initialize project structure with backend and frontend apps, add configuration files, and set up shared package

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	apps: [
+		{
+			name: 'backend',
+			script: 'apps/backend/dist/main.js',
+			instances: 'max',
+			exec_mode: 'cluster',
+			env: {
+				NODE_ENV: 'production',
+			},
+		},
+		{
+			name: 'frontend',
+			script: 'apps/frontend/.next/standalone/server.js',
+			instances: 'max',
+			exec_mode: 'cluster',
+			env: {
+				NODE_ENV: 'production',
+			},
+		},
+	],
+};
