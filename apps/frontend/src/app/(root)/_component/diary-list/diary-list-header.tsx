2567edcd6e4a7335abbf34e357c85c4e05e07862
상국 배 <partner.ben@kyobobook.com>
2024-10-04 19:47:17 +0900

    feat: add DiaryListHeader and DiaryListItem components; implement DiaryListPage to include header

---
+++ apps/frontend/src/app/(root)/_component/diary-list/diary-list-header.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils"
+import { useState } from "react"
+
+export const DiaryListHeader = () => {
+  const [sort, setSort] = useState("최신순")
+  return (
+    <header className="flex justify-between mt-[47px] px-[18px] mb-5">
+      <h2 className="font-bold tracking-[-0.03em] text-black">내일기</h2>
+
+      <ul className="flex items-center gap-x-4">
+        {["최신순", "오래된순", "좋아요순"].map((item) => (
+          <li
+            className={cn(
+              "relative font-medium text-sm tracking-[-0.03em] text-black",
+              sort === item ? "text-black" : "text-[#c3c3c3]",
+              {
+                "before:absolute before:top-1/2 before:block before:w-[1px] before:-translate-y-1/2 before:transform before:bg-[#d5d5d5] before:content-[''] before:left-[-8px] before:h-5":
+                  item !== "최신순",
+              }
+            )}
+            key={item}
+            onClick={() => setSort(item)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </header>
+  )
+}
---
+++ apps/frontend/src/app/(root)/diary-list/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { DiaryListHeader } from "../_component/diary-list/diary-list-header"
+
+// import { DiaryListItem } from "../_component/diary-list/diary-list-item"
+
+export default function DiaryListPage() {
+  return (
+    <div className="">
+      <DiaryListHeader />
+      <div className="p-5">{/* <DiaryListItem /> */}</div>
+    </div>
+  )
+}
