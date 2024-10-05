39c68eae9adedfd534ad77158c35e77775a4f828
Park Na Jin <cosmoss9199@gmail.com>
2024-10-06 02:25:32 +0900

    response log 추가

--- apps/backend/src/middleware/logger.middleware.ts
+++ apps/backend/src/middleware/logger.middleware.ts
@@ -9,6 +9,47 @@ export class LoggerMiddleware implements NestMiddleware {
 
     console.log(`[${timestamp}] ${method} ${originalUrl}`)
 
+    const originalSend = res.send.bind(res)
+
+    res.send = (body) => {
+      // 응답 본문이 문자열일 경우에만 JSON.parse 가능
+      if (typeof body === "string") {
+        try {
+          const responseBody = JSON.parse(body)
+          res.on("finish", () => {
+            const responseTime = new Date().toISOString()
+            const statusCode = res.statusCode
+            console.log(
+              `[${responseTime}] ${method} ${originalUrl} - Status: ${statusCode} - Response:`,
+              responseBody
+            )
+          })
+        } catch (e) {
+          res.on("finish", () => {
+            const responseTime = new Date().toISOString()
+            const statusCode = res.statusCode
+            console.log(
+              `[${responseTime}] ${method} ${originalUrl} - Status: ${statusCode} - Response:`,
+              body
+            )
+            // JSON 파싱 실패 시 처리
+            console.log(`[Error] : ${e}`)
+          })
+        }
+      } else {
+        res.on("finish", () => {
+          const responseTime = new Date().toISOString()
+          const statusCode = res.statusCode
+          console.log(
+            `[${responseTime}] ${method} ${originalUrl} - Status: ${statusCode} - Response:`,
+            body
+          )
+        })
+      }
+
+      return originalSend(body)
+    }
+
     next()
   }
 }
