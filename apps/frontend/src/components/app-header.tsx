dddf7da10f640d6503b4dd1bcc1dc2dd557618d4
상국 배 <partner.ben@kyobobook.com>
2024-10-06 15:09:49 +0900

    Refactor AppHeader to simplify navigation logic and remove unused alert functionality

--- apps/frontend/src/components/app-header.tsx
+++ apps/frontend/src/components/app-header.tsx
@@ -1,14 +1,11 @@
 "use client"
 
-import { useRouter } from "next/navigation"
-import { useAlert } from "@/store/alert"
-import { format } from "date-fns"
-import { ko } from "date-fns/locale"
-
-import { convertKRDate } from "@/lib/utils"
-
 import { ButtonGroup } from "./button-group"
 import { Icons } from "./ui/icons"
+import { convertKRDate } from "@/lib/utils"
+import { format } from "date-fns"
+import { ko } from "date-fns/locale"
+import { useRouter } from "next/navigation"
 
 export const AppHeader = ({
   date,
@@ -17,24 +14,14 @@ export const AppHeader = ({
   date: string
   diaryId: number
 }) => {
-  const { setAlert } = useAlert()
   const router = useRouter()
-  const isEditing = true
+
   return (
     <div className="relative flex justify-center items-center w-full mt-[35px] h-[46px]">
       <Icons.chevronLeft
         className="absolute left-[18px] cursor-pointer"
         onClick={() => {
-          if (isEditing) {
-            setAlert({
-              open: true,
-              title: "수정을 중단할까요?",
-              description: "수정 중인 내용이 원래대로 돌아가요",
-              cancelName: "계속 작성",
-              confirmName: "나가기",
-              confirmAction: () => router.push("/diary"),
-            })
-          }
+          router.push("/diary")
         }}
       />
       <div className="text-center font-bold tracking-[-0.03em]">
